4d3219bba0d96bfee9e64d0e0fc388d93d429562
hadar18-meet <hadarky@gmail.com>
2017-03-06 19:55:08 +0200

    bvcx

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -10,11 +10,11 @@ my_phonebook={"Statue of Liberty":2125555555, "Ghostbusters":2125551234}
 ###
 #Print the variable, num
 ###
-
+my_phonebook["Statue of Liberty"]
 ###
 #Change the value of the Ghostbusters' phone number to 2125559876
 ###
-
+my_phonebook["Ghostbusters"] = 2125559876
 #Here's an empty dictionary
 your_phonebook={}
 
@@ -22,6 +22,11 @@ your_phonebook={}
 #Add 5 values to it like this
 #your_phonebook['key']=value
 ###
+your_phonebook[1]="1"
+your_phonebook[2]="2"
+your_phonebook[3]="3"
+your_phonebook[4]="4"
+your_phonebook[5]="5"
 
 ###
 #Use the keys() method: your_phonebook.keys()
@@ -32,7 +37,8 @@ your_phonebook={}
 
 #for key in sequence_of_keys :
 #    #Do stuff
-
+for key in your_phonebook:
+    print( str(key) + ":" + your_phonebook[key])
 #Use the loop to print to the shell the key and value
 #of every element in the dictionary
 ###
